6fc7400bb14bad50e5b69d350c4c75e743c22e03
Gustavo <141040266+GustavoFiguerido1010@users.noreply.github.com>
2023-09-11 19:18:22 -0300

    Add commits of product and css star

--- product-info.js
+++ product-info.js
@@ -45,3 +45,76 @@ function displayProductDetails(productData) {
     //esto si se coloca arriba no iba a estar en el html por eso se coloca luego del get ele by id prod detail cont
   }
 }
+
+// funcion para mostrar comentarios //
+
+document.addEventListener("DOMContentLoaded", function () {
+  const selectedProductCommentsId = localStorage.getItem("prodId");
+
+  if (selectedProductCommentsId) {
+    const PRODUCTCOMMENTS_URL = `https://raw.githubusercontent.com/JaPCeibal/emercado-api/main/products_comments/${selectedProductCommentsId}.json`;
+
+    fetch(PRODUCTCOMMENTS_URL)
+      .then(response => response.json())
+      .then(product_commentsData => {
+        // Mostrar los comentarios del producto en la página
+        displayProductComments(product_commentsData);
+      })
+      .catch(error => {
+        console.error("Error al cargar los datos del producto", error);
+      });
+  } else {
+    // Si no se ha seleccionado un producto, mostrar un mensaje de error o redirigir a otra página
+    document.getElementById("product-details-comments").innerHTML = "<p>No se ha seleccionado ningún producto.</p>";
+  }
+});
+
+//////////// funcion para calificacion con estrellas //////////////
+
+function createStarRating(score) {
+  const starCount = 5; // Número total de estrellas
+  const filledStars = Math.round(score); // Número de estrellas llenas. La funcion Math.round rendea a numeros enteros.
+
+  let starRatingHTML = ''; // Cadena HTML para las estrellas
+
+  // Creamos estrellas llenas
+  for (let i = 0; i < filledStars; i++) {
+    starRatingHTML += '<i class="fa fa-star"></i>'; // fa fa-star hace referencia a estrellas llenas
+  }
+
+  // Creamos las estrellas vacías (las que faltan)
+  for (let i = filledStars; i < starCount; i++) {
+    starRatingHTML += '<i class="fa fa-star-o"></i>'; // fa fa-star-o hace referencia a estrellas vacias
+  }
+
+  return starRatingHTML; // Devolver la cadena HTML de las estrellas
+}
+
+
+
+function displayProductComments(commentsData) {
+  // Verifica si hay comentarios en los datos
+  if (commentsData && commentsData.length > 0) {
+    // Muestra los comentarios del producto
+    let commentsHTML = "<p><strong>Comentarios:</strong></p>";
+
+    commentsData.forEach(comment => {
+      const starRatingHTML = createStarRating(comment.score); // Se crea la calificación con estrellas
+
+      commentsHTML += `
+        <div class="comment">
+          <p><span class="user">${comment.user}</span> - ${comment.dateTime} <div class="star-rating">${starRatingHTML}</div></p>
+          <p>${comment.description}</p> 
+        </div>
+      `;
+    });
+
+    document.getElementById("product-details-comments").innerHTML = commentsHTML;
+  } else {
+    console.error("No hay comentarios disponibles para este producto.");
+  }
+}
+
+
+
+
