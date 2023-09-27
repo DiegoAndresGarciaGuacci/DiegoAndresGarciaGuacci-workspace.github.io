224c078a83b0e3dbd519641b96ccdff37cafeb82
Gustavo <141040266+GustavoFiguerido1010@users.noreply.github.com>
2023-09-27 10:04:59 -0300

    carrusel

--- js/product-info.js
+++ js/product-info.js
@@ -1,54 +1,97 @@
-
-
 document.addEventListener("DOMContentLoaded", function () {
   const selectedProductId = localStorage.getItem("prodId");
 
-
   if (selectedProductId) {
-  
-      const PRODUCT_URL = `https://japceibal.github.io/emercado-api/products/${selectedProductId}.json`;
+    const PRODUCT_URL = `https://japceibal.github.io/emercado-api/products/${selectedProductId}.json`;
 
-      fetch(PRODUCT_URL)
-          .then(response => response.json())
-          .then(productData => {
-              // Mostrar la información del producto en la página
-              displayProductDetails(productData);
-          })
-          .catch(error => {
-              console.error("Error al cargar los datos del producto", error);
-          });
+    fetch(PRODUCT_URL)
+      .then(response => response.json())
+      .then(productData => {
+        // Mostrar la información del producto en la página
+        displayProductDetails(productData);
+        // Crear el carrusel con las imágenes del producto
+        ImageCarousel(productData.images);
+      })
+      .catch(error => {
+        console.error("Error al cargar los datos del producto", error);
+      });
   } else {
-      // Si no se ha seleccionado un producto, mostrar un mensaje de error o redirigir a otra página
-      document.getElementById("product-details-container").innerHTML = "<p>No se ha seleccionado ningún producto.</p>";
+    // Si no se ha seleccionado un producto, mostrar un mensaje de error o redirigir a otra página
+    document.getElementById("product-details-container").innerHTML = "<p>No se ha seleccionado ningún producto.</p>";
   }
 });
 
 function displayProductDetails(productData) {
   // Crear el contenido HTML para mostrar los detalles del producto
   const productHTML = `
-      <h1 class="productTitulo">${productData.name}</h1>
-      </br>
-      </br>
-      </br>
-      </br>
-      <div class="productInfo"
-      <p><strong>Imagenes ilustrativas:</strong></p>
+    <h1 class="productTitulo">${productData.name}</h1>
+    </br>
+    <div class="productInfo">
+      <h3><strong>Imagenes ilustrativas:</strong></h3>
       </br>
       <div id="img" class="img"></div>
+      <div class="controles">
+      <button id="anterior" class="boton-control"><i  class="bi bi-arrow-left-circle-fill"></i></button>
+      <button id="siguiente" class="boton-control"><i class="bi bi-arrow-right-circle-fill"></i></button>
+    </div>
+      </br>
       </br>
       <p><strong>Precio:</strong> ${productData.currency} ${productData.cost}</p>
       <p><strong>Descripción:</strong> ${productData.description}</p>
       <p><strong>Cantidad Vendidos:</strong> ${productData.soldCount}</p>
       <p><strong>Categoria:</strong> ${productData.category}</p>
-      </div>
+    </div>
+    
   `;
   document.getElementById("product-details-container").innerHTML = productHTML;
-  for (let i = 0; i < productData.images.length; i++){ //
-    let img = `<img src="${productData.images[i]}">` //comillas para definir un string 
-    document.getElementById("img").innerHTML += img
-    //esto si se coloca arriba no iba a estar en el html por eso se coloca luego del get ele by id prod detail cont
-  }
 }
+
+function ImageCarousel(images) {
+  
+  // Obtén referencias a elementos HTML
+
+  const imgContainer = document.getElementById("img");
+  const anteriorBtn = document.getElementById("anterior");
+  const siguienteBtn = document.getElementById("siguiente");
+
+  // Configura variables de control
+  let currentIndex = 0;
+
+  // Función para cambiar al siguiente elemento del carrusel
+  function cambiarSiguiente() {
+    currentIndex++;
+    if (currentIndex === images.length) {
+      currentIndex = 0; // Vuelve al primer elemento cuando llegas al final
+    }
+    actualizarCarrusel();
+  }
+
+  // Función para cambiar al elemento anterior del carrusel
+  function cambiarAnterior() {
+    currentIndex--;
+    if (currentIndex < 0) {
+      currentIndex = images.length - 1; // Vuelve al último elemento cuando llegas al principio
+    }
+    actualizarCarrusel();
+  }
+
+  // Función para actualizar la vista del carrusel
+  function actualizarCarrusel() {
+    imgContainer.innerHTML = `<img src="${images[currentIndex]}">`;
+  }
+
+  // Agregar eventos de clic a los botones de control
+  anteriorBtn.addEventListener("click", cambiarAnterior);
+  siguienteBtn.addEventListener("click", cambiarSiguiente);
+
+  // Mostrar la primera imagen
+  actualizarCarrusel();
+
+// Obtén la referencia a la imagen
+const imgElement = document.querySelector("#img img");
+
+}
+
 
 
 /*-----------------------------------*\
@@ -231,7 +274,3 @@ userInfoElement.textContent = ` ${savedUsername}`;
        body.classList.add('dark-theme');
      }
    });
- 
-
-  
-  
