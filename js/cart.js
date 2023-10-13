e2c8e2cdf12afb20b8e19ccd1ad3c34ceec19979
maddccr <128443739+maddccr@users.noreply.github.com>
2023-10-13 08:37:24 -0300

    Update cart.js
    Cost!

--- js/cart.js
+++ js/cart.js
@@ -61,7 +61,7 @@ function displayProducts(data) {
             class="img-fluid rounded-3" alt="Producto">
         </div>
         <div class="col-md-3 col-lg-3 col-xl-3">
-          <h6 class="text-muted">Producto</h6>
+          <h6 class="text-muted">${product.unitCost} ${product.currency}</h6>
           <h6 class="text-black mb-0">${product.name}</h6>
         </div>
         <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
